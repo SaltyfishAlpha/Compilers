e7cce4c0bb9562b65eb8d31cbe18d10d01f7d9db
SaltyfishAlpha <saltyfishalpha@gmail.com>
2023-03-07 00:04:27 +0800

    delete test code

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,22 @@
 
 #include "Parser.h"
 
-std::string program1 = 
-"# Compute the x'th fibonacci number.\n"
-"def fib(x)\n"
-"  if x < 3 then\n"
-"    1\n"
-"  else\n"
-"    fib(x-1)+fib(x-2)\n"
-"# This expression will compute the 40th number.\n"
-"fib(40)\n";
+// std::string program1 = 
+// "# Compute the x'th fibonacci number.\n"
+// "def fib(x)\n"
+// "  if x < 3 then\n"
+// "    1\n"
+// "  else\n"
+// "    fib(x-1)+fib(x-2)\n"
+// "# This expression will compute the 40th number.\n"
+// "fib(40)\n";
 
-std::string program2 =
-"extern sin(arg);\n"
-"extern cos(arg);\n"
-"extern atan2(arg1 arg2);\n"
-"atan2(sin(.4), cos(42))"
-;
+// std::string program2 =
+// "extern sin(arg);\n"
+// "extern cos(arg);\n"
+// "extern atan2(arg1 arg2);\n"
+// "atan2(sin(.4), cos(42))"
+// ;
 
 static void HandleDefinition() {
     if (ParseDefinition()) {
